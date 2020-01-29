ac8c03d9d429b8c3293dc5a42b3e897a92a2035c
Mohamed Abdelsalam <mohamedabdelsalam9499@gmail.com>
2020-01-30 00:47:52 +0200

    last try

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -8,6 +8,8 @@ DEBUG = True
 
 # Connect to the database
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = "postgres://{}:{}@{}/{}".format('postgres', '123456', 'localhost:5432', 'fyyurdb')
